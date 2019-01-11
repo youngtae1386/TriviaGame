87b0f034632625b23548cd13083fdb3094046c2d
Young Cho <youngtae1386@gmail.com>
2019-01-11 17:55:08 -0600

    checking jpg and css fonts and styling

--- assets/libraries/main.js
+++ assets/libraries/main.js
@@ -1,207 +1,205 @@
-
-
-
-var timeleft = 50;
-var downloadTime = setInterval(function () {
-    timeleft--;
-    document.getElementById("countDownTimer").textContent = timeleft;
-    document.getElementById("progressBar").value = 50 - timeleft;
-    if (timeleft <= 0)
-        clearInterval(downloadTime);
-}, 1000);
-
-
-
 $(document).ready(function () {
     var options = [
         {
-            question: "How old is the universe??", 
+            question: "How old is the universe??",
             choice: ["15-20 billion years old", "20-25 billion years old", "1-5 billion years old", "100 million years old"],
             answer: 0,
-            photo: "assets/images/pupusas.jpg"
-         },
-         {
-             question: "How old is the earth?", 
+            photo: "assets/images/Galaxy-Universe.jpg"
+        },
+        {
+            question: "How old is the earth?",
             choice: ["about 3.9 billion years old", "about 4.5 billion years old", "about 1 billion years old", "900 million years old"],
             answer: 1,
-            photo: "assets/images/mtdew.gif"
-         }, 
-         {
-             question: "What explorer introduced pigs to North America?", 
-            choice: ["Johnson Columbus", "Viking", "Indian", "Christopher Columbus" ],
+            photo: "assets/images/earth.jpg"
+        },
+        {
+            question: "What explorer introduced pigs to North America?",
+            choice: ["Johnson Columbus", "Viking", "Indian", "Christopher Columbus"],
             answer: 3,
-            photo: "assets/images/coffee.gif"
-        }, 
+            photo: "assets/images/ChristopherColumbusPig.jpg"
+        },
         {
-            question: "Which planet is closest to the sun?", 
-            choice: ["Earth", "Mercury", "Mars", "Moon" ],
+            question: "Which planet is closest to the sun?",
+            choice: ["Earth", "Mercury", "Mars", "Moon"],
             answer: 1,
-            photo: "assets/images/harvey.jpg"
-        }, 
+            photo: "assets/images/closestToSun.jpg"
+        },
         {
-            question: "What’s the most malleable metal?", 
-            choice: ["silver", "steel", "metal", "gold" ],
+            question: "What’s the most malleable metal?",
+            choice: ["silver", "steel", "metal", "gold"],
             answer: 3,
-            photo: "assets/images/dozen.jpg"
-        }, 
+            photo: "assets/images/gold.jpg"
+        },
         {
-            question: "What is the most widely eaten fish in the world?", 
-            choice: ["Tilapia", "Herring", "Sardine", "Tuna" ],
-            answer: 1,
-            photo: "assets/images/herring.jpg"
-        }, 
+            question: "About how much trash does the world produce a year?",
+            choice: ["1 million", "1,000 million tons", "100 million tons", " 250 million tons"],
+            answer: 3,
+            photo: "assets/images/trash.jpg"
+        },
         {
-            question: "What do doctors look at through an ophthalmoscope?", 
-            choice: ["The eye", "The Ear", "Mouth", "Chest" ],
+            question: "What do doctors look at through an ophthalmoscope?",
+            choice: ["eye", "Ear", "Mouth", "Chest"],
             answer: 0,
-            photo: "assets/images/lemon.gif"
-        }, 
-        {
-            question: "Which fruit contains the most protein per 100 calories?", 
-            choice: ["Guava", "Avocado", "Banana", "Blackberries" ],
-            answer: 0,
-            photo: "assets/images/guava.gif"
+            photo: "assets/images/eye.JPG"
         }];
 
+
+
+    var rightCount = 0;
+    var wrongCount = 0;
+    var unansweredCount = 0;
+    var timer = 10;
+    var intervalId;
+    var userGuess = "";
+    var running = false;
+    var qCount = options.length;
+    var pick;
+    var index;
+    var newArray = [];
+    var holder = [];
+
+
+    $(".progress").hide();
+    $("#playOver").hide();
     
+    
+    //click to start
+    $("#start").on("click", function () {
+        runTimer();
+        $("#start").hide();
 
-        $("#reset").hide();
-        //click start button to start game
-        $("#start").on("click", function () {
-                $("#start").hide();
-                displayQuestion();
-                runTimer();
-                for(var i = 0; i < options.length; i++) {
+        showQuestion();
+
+        
+        for (var i = 0; i < options.length; i++) {
             holder.push(options[i]);
+            $(".progress").show();
+        }
+    })
+    //timer start
+    function runTimer() {
+        if (!running) {
+            intervalId = setInterval(decrement, 1000);
+            running = true;
+        }
+    }
+    //timer countdown
+    var progressBar = 100;
+    function decrement() {
+        $("#time").html("<h3>Time remaining: " + timer + "</h3>");
+        $(".progress").html
+            ('<div class="progress-bar progress-bar-striped progress-bar-animated"  role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="20" style=width:' + progressBar + "%></div>");
+        // x = timer/-4;
+        timer--;
+        progressBar -= 10;
+
+
+        //stop timer if reach 0 actullay -1
+        if (timer === -1) {
+            unansweredCount++;
+            stop();
+            $("#answerblock").html("<p>Time is up! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+            hidepicture();
+        }
+    }
+
+    //timer stop
+    function stop() {
+        running = false;
+        clearInterval(intervalId);
+    }
+   
+    //display question
+    function showQuestion() {
+        //generate random
+        index = Math.floor(Math.random() * options.length);
+        pick = options[index];
+
+
+        $("#questionSections").html("<h2>" + pick.question + "</h2>");
+        //loop though displayed answers
+        for (var i = 0; i < pick.choice.length; i++) {
+            var userChoice = $("<div>");
+            userChoice.addClass("answerchoice");
+            userChoice.html(pick.choice[i]);
+         
+            userChoice.attr("data-guessvalue", i);
+            $("#answerblock").append(userChoice);
+            //		}
         }
 
-        //count down the timer start
-        var correctCount = 0;
-        var wrongCount = 0;
-        var unanswerCount = 0;
-        var timer = 20;
-        var intervalId;
-        var userGuess ="";
-        var running = false;
-        var qCount = options.length;
-        var pick;
-        var index;
-        var newArray = [];
-        var holder = [];
 
-        function runTimer() {
-            if (!running) {
-                intervalid = setInterval(decrement, 1000);
-                running = true
+
+        //click function to select answer and outcomes
+        $(".answerchoice").on("click", function () {
+            //grab array position from userGuess
+            userGuess = parseInt($(this).attr("data-guessvalue"));
+
+            //correct guess or wrong guess outcomes
+            if (userGuess === pick.answer) {
+                stop();
+                rightCount++;
+                userGuess = "";
+                $("#answerblock").html("<p>Correct!</p>");
+                hidepicture();
+
+            } else {
+                stop();
+                wrongCount++;
+                userGuess = "";
+                $("#answerblock").html("<p>Wrong! The correct answer is: " + pick.choice[pick.answer] + "</p>");
+                hidepicture();
             }
-          }
-          function decrement() {
-              $("#timeleft").html("<h3>Time remaining: " + timer + timer + "<h3>");
-              timer --;
-
-              // stop timer if reach 0
-              if (timer === 0) {
-                  unanswerCount++;
-                  stop();
-                  $("#answerblock").html("<p> Time is up! The Correct Answer: " + pick.choice[pic.answer] + "</p>");
-                  hidepicture();
-              }
-            }
-function
-
-
-
-
-
-
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
-
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton) {
-
-    function showQuestions(questions, quizContainer) {
-        // we'll need a place to store the output and the answer choices
-        var output = [];
-        var answers;
-
-        // for each question...
-        for (var i = 0; i < questions.length; i++) {
-
-            // first reset the list of answers
-            answers = [];
-
-            // for each available answer...
-            for (letter in questions[i].answers) {
-
-                // ...add an html radio button
-                answers.push(
-                    '<label>'
-                    + '<input type="radio" name="question' + i + '" value="' + letter + '">'
-                    + letter + ': '
-                    + questions[i].answers[letter]
-                    + '</label>'
-                );
-            }
-
-            // add this question and its answers to the output
-            output.push(
-                '<div class="question">' + questions[i].question + '</div>'
-                + '<div class="answers">' + answers.join('') + '</div>'
-            );
-        }
-
-        // finally combine our output list into one string of html and put it on the page
-        quizContainer.innerHTML = output.join('');
+        })
     }
 
 
-    function showResults(questions, quizContainer, resultsContainer) {
+    function hidepicture() {
+        $("#answerblock").append("<img src=" + pick.photo + ">");
+        newArray.push(pick);
+        options.splice(index, 1);
 
-        // gather answer containers from our quiz
-        var answerContainers = quizContainer.querySelectorAll('.answers');
+        var hidpic = setTimeout(function () {
+            $("#answerblock").empty();
+            timer = 10;
+            progressBar = 100;
 
-        // keep track of user's answers
-        var userAnswer = '';
-        var numCorrect = 0;
+            // $('body').css("background-image", "url(assets/images/Universe-Stars.JPG)");  
+            // $('.wrapper').css("background-image", "url(assets/images/goldenBridge.jpg)");  
 
-        // for each question...
-        for (var i = 0; i < questions.length; i++) {
+            //run the score screen if all questions answered
+            if ((wrongCount + rightCount + unansweredCount) === qCount) {
+                $("#questionSections").empty();
+                $("#questionSections").html("<h3>Game Over! </h3>");
+                $("#answerblock").append("<h4> Correct: " + rightCount + "</h4>");
+                $("#answerblock").append("<h4> Incorrect: " + wrongCount + "</h4>");
+                $("#answerblock").append("<h4> Unanswered: " + unansweredCount + "</h4>");
+                $("#playOver").show();
+                rightCount = 0;
+                wrongCount = 0;
+                unansweredCount = 0;
 
-            // find selected answer
-            userAnswer = (answerContainers[i].querySelector('input[name=question' + i + ']:checked') || {}).value;
-
-            // if answer is correct
-            if (userAnswer === questions[i].correctAnswer) {
-                // add to the number of correct answers
-                numCorrect++;
-
-                // color the answers green
-                answerContainers[i].style.color = 'green';
+            } else {
+                runTimer();
+                showQuestion();
 
             }
-            // if answer is wrong or blank
-            else {
-                // color the answers red
-                answerContainers[i].style.color = 'red';
-                answerContainers[i].style.background = 'black';
-                answerContainers[i].style.display = 'inline-block';
-                answerContainers[i].style.width = '28%';
-            }
+        }, 3000);
+
+
+    }
+
+    $("#playOver").on("click", function () {
+        $("#playOver").hide();
+        $("#questionSections").empty();
+        $("#answerblock").empty();
+       
+        for (var i = 0; i < holder.length; i++) {
+            options.push(holder[i]);
         }
+        runTimer();
+        showQuestion();
 
-        // show number of correct answers out of total
-        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
-    }
+    })
 
-    // show questions right away
-    showQuestions(questions, quizContainer);
-
-    // on submit, show results
-    submitButton.onclick = function () {
-        showResults(questions, quizContainer, resultsContainer);
-    }
-
-}
+})
